d553afebde1f7f707feafd2ffc7a2d4193c6b1b9
Marcus Wetterbro <wetterbro@gmail.com>
2024-04-23 13:17:40 +0200

    update

--- taskmanagementsystem/src/app/page.js
+++ taskmanagementsystem/src/app/page.js
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
     <main className="">
-     
+     <nav>
+      <h1>Task Management System</h1>
+      </nav>
+
+      <section>
+      <h1>Add form</h1> 
+      </section>
+
+
+      <section>
+      <h1>sorting</h1> 
+      </section>
+
+
+      <section>
+      <h2>Task List</h2>
+      </section>
+    
     </main>
   );
 }
